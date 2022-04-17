568d8c0502bedc01bf3a1b2fe1541ff5f27a628e
Arda Şen <asen436@gmail.com>
2022-04-17 13:30:44 +0300

    Update device.mk
    Changes for DotOS

--- device.mk
+++ device.mk
@@ -4,10 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-AB_OTA_UPDATER := true
+# Device Path
+DEVICE_PATH := device/xiaomi/jasmine_sprout
 
 # Inherit from wayne-common
 $(call inherit-product, device/xiaomi/wayne-common/wayne.mk)
+
+# A/B
+ENABLE_AB := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
@@ -17,44 +21,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script
-
-# Boot control
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl:64 \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service \
-    bootctrl.sdm660 \
-    bootctrl.sdm660.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
-
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.verity.rc
-
-# Remove few packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
-# Update engine
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
