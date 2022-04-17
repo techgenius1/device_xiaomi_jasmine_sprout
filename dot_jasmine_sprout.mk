48b2d10dc8906b47380812daf81d9c347578ca71
Arda Şen <asen436@gmail.com>
2022-04-17 13:19:03 +0300

    Update and rename derp_jasmine_sprout.mk to dot_jasmine_sprout.mk
    Changes for DotOS

--- dot_jasmine_sprout.mk
+++ dot_jasmine_sprout.mk
@@ -10,19 +10,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
 # Inherit from jasmine_sprout device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
-
-USE_LEGACY_BOOTANIMATION := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi A2
-PRODUCT_NAME := derp_jasmine_sprout
+PRODUCT_NAME := dot_jasmine_sprout
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
